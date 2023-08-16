261a706feb4530bae458aba9eb541731e7a1a54f
Potter72 <elliot.potter72@gmail.com>
2023-08-16 16:27:35 +0200

    maybe a bit wokring

--- Assets/Scripts/Elliot/RopeGenerator.cs
+++ Assets/Scripts/Elliot/RopeGenerator.cs
@@ -17,16 +17,17 @@ public class RopeGenerator : NetworkBehaviour
 
     [SerializeField] Rigidbody2D rbOne;
     [SerializeField] Rigidbody2D rbTwo;
+    [SerializeField] Transform ropeParent;
 
-    public override void OnNetworkSpawn()
+    void Start()
     {
         StartCoroutine(GenerateRope());
     }
 
     IEnumerator GenerateRope()
     {
-        if (!IsServer) yield break;
-        yield return new WaitForSeconds(4f);
+        //if (!IsServer) yield break;
+        //yield return new WaitForSeconds(4f);
         Vector3 vectorBetweenPlayers = player2.position - player1.position;
         float distanceBetweenPlayers = vectorBetweenPlayers.magnitude;
 
@@ -40,8 +41,12 @@ public class RopeGenerator : NetworkBehaviour
             spawnPos = Vector3.Lerp(player1.position + vectorBetweenPlayers.normalized * distanceBetweenPlayers / numberOfJoints, player2.position - vectorBetweenPlayers.normalized * distanceBetweenPlayers / numberOfJoints, lerpValue);
             //spawnPos = Vector3.Lerp(player2.position, player1.position , lerpValue);
             //Vector3 spawnPos = player1.transform.position + vectorBetweenPlayers.normalized * distanceBetweenPlayers * ((i + 1) / numberOfJoints);
+
             GameObject spawnedObject = Instantiate(ropeJointPrefab, spawnPos, Quaternion.identity);
-            spawnedObject.GetComponent<NetworkObject>().Spawn();
+            spawnedObject.transform.SetParent(ropeParent,true);
+
+            //spawnedObject.GetComponent<NetworkObject>().Spawn();
+
             DistanceJoint2D joint = spawnedObject.GetComponent<DistanceJoint2D>();
 
             if (i == 0)
@@ -54,13 +59,12 @@ public class RopeGenerator : NetworkBehaviour
             }
             previousRb = spawnedObject.GetComponent<Rigidbody2D>();
             joint.distance = distanceBetwenJoints;
+            
 
             yield return new WaitForSeconds(timeBetweenRopeLoad);
         }
 
         player2.GetComponent<DistanceJoint2D>().connectedBody = previousRb;
         player2.GetComponent<DistanceJoint2D>().distance = distanceBetwenJoints;
-        rbOne.gravityScale = 1.0f;
-        rbTwo.gravityScale = 1.0f;
     }
 }
