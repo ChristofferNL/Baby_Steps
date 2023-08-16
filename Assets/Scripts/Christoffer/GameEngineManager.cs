e21a39054b601169337ba642964765c31f79263f
Potter72 <elliot.potter72@gmail.com>
2023-08-16 12:07:03 +0200

    procedural rope

---
+++ Assets/Scripts/Christoffer/GameEngineManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.Netcode;
+using Unity.Services.Lobbies.Models;
+using UnityEngine;
+
+public class GameEngineManager : NetworkBehaviour
+{
+	public void HandlePlayerInput(Rigidbody2D rigidbody,
+								float moveInput,
+								float jumpForce,
+								InputManager.JumpDirection jumpDirection)
+	{
+		MovePlayer(moveInput, jumpForce, jumpDirection, rigidbody); // for testing
+
+		if (!IsHost) return;
+		MovePlayer(moveInput, jumpForce, jumpDirection, rigidbody);
+	}
+
+	void MovePlayer(float moveInput, float jumpForce, InputManager.JumpDirection jumpDirection, Rigidbody2D rigidbody)
+	{
+
+		if (moveInput != 0)
+		{
+			rigidbody.AddForce(new Vector2(moveInput, 0), ForceMode2D.Force);
+		}
+		if (jumpForce > 0)
+		{
+            Debug.Log(jumpForce);
+            switch (jumpDirection)
+			{
+				case InputManager.JumpDirection.NONE:
+					rigidbody.AddForce(new Vector2(0, 1) * jumpForce, ForceMode2D.Impulse);
+					break;
+				case InputManager.JumpDirection.LEFT:
+					rigidbody.AddForce(new Vector2(-1, 2) * jumpForce, ForceMode2D.Impulse);
+					break;
+				case InputManager.JumpDirection.RIGHT:
+					rigidbody.AddForce(new Vector2(1, 2) * jumpForce, ForceMode2D.Impulse);
+					break;
+				default:
+					break;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Christoffer/InputManager.cs
@@ -0,0 +1,131 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class InputManager : NetworkBehaviour
+{
+	public enum JumpDirection
+	{
+		NONE,
+		LEFT,
+		RIGHT,
+	}
+	JumpDirection jumpDirection;
+
+	[SerializeField] GameEngineManager manager;
+	[SerializeField] Rigidbody2D playerRb;
+	[SerializeField] float moveForce;
+	[SerializeField] float jumpForceToAdd;
+	[SerializeField] float jumpForceBase;
+	[SerializeField] float timeBetweenAdd;
+	[SerializeField] float jumpForceTimesToAdd;
+	[SerializeField] float groundCheckDistance;
+	[SerializeField] LayerMask groundCheckLayerMask;
+
+	PlayerInputs inputActions;
+	PlayerInputs.PlayerActionMapActions actions;
+
+	public bool GameIsRunning;
+	private bool chargingJump;
+	public bool IsGrounded;
+	private bool doGroundCheck = true;
+
+	private void Awake()
+	{
+		inputActions = new();
+		actions = inputActions.PlayerActionMap;
+	}
+
+	private void OnEnable()
+	{
+		actions.Enable();
+	}
+
+	private void OnDisable()
+	{
+		actions.Disable();
+	}
+
+	private void Update()
+	{
+		if (!GameIsRunning) return;
+		GroundCheck();
+		GetInputs();
+	}
+
+	private void GroundCheck()
+	{
+		if (!doGroundCheck) return;
+		if (Physics2D.Raycast(playerRb.transform.position, Vector2.down, groundCheckDistance, groundCheckLayerMask))
+		{
+			IsGrounded = true;
+		}
+		else
+		{
+			IsGrounded = false;
+		}
+	}
+
+	private void GetInputs()
+	{
+		switch (actions.Move.ReadValue<float>())
+		{
+			case < 0:
+				jumpDirection = JumpDirection.LEFT;
+				break;
+			case > 0:
+				jumpDirection = JumpDirection.RIGHT;
+				break;
+			default:
+				jumpDirection = JumpDirection.NONE;
+				break;
+		}
+
+		if (actions.Jump.IsPressed() && IsGrounded)
+		{
+			chargingJump = true;
+			StartCoroutine(ChargeJump());
+		}
+		else if (actions.Jump.WasReleasedThisFrame())
+		{
+			chargingJump = false;
+		}
+
+		IEnumerator ChargeJump()
+		{
+			float jumpForce = jumpForceBase;
+			int counter = 0;
+			while (chargingJump && IsGrounded)
+			{
+				if (counter <= jumpForceTimesToAdd)
+				{
+					counter++;
+					jumpForce += jumpForceToAdd;
+				}
+				yield return new WaitForSeconds(timeBetweenAdd);
+			}
+
+			if (IsGrounded)
+			{
+				manager.HandlePlayerInput(playerRb, actions.Move.ReadValue<float>() * moveForce, jumpForce, jumpDirection);
+				StartCoroutine(StopGroundCheckForTime(0.7f));
+                yield break;
+			}
+		}
+
+		if (!chargingJump)
+		{
+			manager.HandlePlayerInput(playerRb, actions.Move.ReadValue<float>() * moveForce, 0, jumpDirection);
+		}
+	}
+
+	IEnumerator StopGroundCheckForTime(float time)
+	{
+		IsGrounded = false;
+		doGroundCheck = false;
+        yield return new WaitForSeconds(time);
+        doGroundCheck = true;
+    }
+}
