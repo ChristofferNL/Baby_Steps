d05d98b1fb53b9beee3e746870d1bfecc224bb25
Potter72 <elliot.potter72@gmail.com>
2023-08-22 14:30:38 +0200

    better level data generator +spawning

--- Assets/Scripts/Elliot/LevelGenerator.cs
+++ Assets/Scripts/Elliot/LevelGenerator.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using Unity.Netcode;
+using Unity.Netcode.Components;
 using UnityEngine;
 using UnityEngine.Pool;
 
@@ -25,6 +27,12 @@ public class LevelGenerator : NetworkBehaviour
     private List<GameObject> spawnedObjects;
 
     public List<int> levelIdOrder = new List<int>();
+    public List<int> levelSpawnOrder = new List<int>();
+    public List<GameObject> spawnedChunk1 = new List<GameObject>();
+    public List<GameObject> spawnedChunk2 = new List<GameObject>();
+    public List<GameObject> spawnedChunk3 = new List<GameObject>();
+    [SerializeField] GameObject levelChunkHolder;
+    public int amountOfSpawnedLevels;
 
     public Vector3 nextStartPos;
     public Vector3 oldStartPos;
@@ -36,6 +44,7 @@ public class LevelGenerator : NetworkBehaviour
     [SerializeField] Transform player1;
     [SerializeField] Transform player2;
     [SerializeField] Transform originalAnchorPos;
+
 
     private void Start()
     {
@@ -106,43 +115,155 @@ public class LevelGenerator : NetworkBehaviour
     void SpawnChunk()
     {
         int levelId = Random.Range(0, chunkDataSOs.Length);
-        Debug.Log("number of platforms:" + chunkDataSOs[levelId].numberOfPlatforms);
+        //Debug.Log("number of platforms:" + chunkDataSOs[levelId].numberOfPlatforms);
 
         if (levelIdOrder.Count < 1)
         {
             nextStartPos = originalAnchorPos.position;
         }
 
+        //destroys the lowest level
+        if(spawnedChunk1.Count != 0 && spawnedChunk2.Count != 0 && spawnedChunk3.Count != 0)
+        {
+
+            //gets the height of the chunk 1
+            float sum1 = 0;
+            for (int i = 0; i < spawnedChunk1.Count; i++)
+            {
+                sum1 += spawnedChunk1[i].transform.position.y;
+            }
+            float heightChunk1 = sum1 / spawnedChunk1.Count;
+
+            //gets the height of the chunk 2
+            float sum2 = 0;
+            for (int i = 0; i < spawnedChunk2.Count; i++)
+            {
+                sum2 += spawnedChunk2[i].transform.position.y;
+            }
+            float heightChunk2 = sum2 / spawnedChunk2.Count;
+
+            //gets the height of the chunk 3
+            float sum3 = 0;
+            for (int i = 0; i < spawnedChunk3.Count; i++)
+            {
+                sum3 += spawnedChunk3[i].transform.position.y;
+            }
+            float heightChunk3 = sum3 / spawnedChunk3.Count;
+
+            float lowestHeight = heightChunk1;
+            if(lowestHeight > heightChunk2) { lowestHeight = heightChunk2; }
+            if(lowestHeight > heightChunk3) { lowestHeight = heightChunk3; }
+
+            //Debug.Log("lowest height: " + lowestHeight);
+
+            if(lowestHeight ==  heightChunk1) 
+            {
+                for (int i = 0; i < spawnedChunk1.Count; i++)
+                {
+                    spawnedChunk1[i].SetActive(false);
+                }
+                spawnedChunk1.Clear();
+                spawnedChunk1 = new List<GameObject>();
+            }
+            else if(lowestHeight == heightChunk2)
+            {
+                for (int i = 0; i < spawnedChunk2.Count; i++)
+                {
+                    spawnedChunk2[i].SetActive(false);
+                }
+                spawnedChunk2.Clear();
+                spawnedChunk2 = new List<GameObject>();
+            }
+            else if (lowestHeight == heightChunk3)
+            {
+                for (int i = 0; i < spawnedChunk3.Count; i++)
+                {
+                    spawnedChunk3[i].SetActive(false);
+                }
+                spawnedChunk3.Clear();
+                spawnedChunk3 = new List<GameObject>();
+            }    
+        }
+
+        int chunkToSaveTo = 0;
+        if(spawnedChunk1.Count == 0) { chunkToSaveTo = 1; }
+        else if(spawnedChunk2.Count == 0) { chunkToSaveTo = 2; }
+        else if(spawnedChunk3.Count == 0) { chunkToSaveTo = 3; }
+
+        Debug.Log("chunktosaveto: " + chunkToSaveTo);
+
+        //GameObject spawnedHolderParent = Instantiate(levelChunkHolder, transform.position, Quaternion.identity);
+
         for (int i = 0; i < chunkDataSOs[levelId].numberOfPlatforms;)
         {
             GameObject spawnedObject;
-            Debug.Log("trying to spawn");
 
             if (chunkDataSOs[levelId].isPassThrough[i])
             {
-                Debug.Log("is pass through");
                 spawnedObject = GetPooledPassTrough();
                 if (spawnedObject == null) return;
                 spawnedObject.transform.localPosition = nextStartPos + chunkDataSOs[levelId].position[i];
                 spawnedObject.transform.localScale = chunkDataSOs[levelId].scale[i];
                 spawnedObject.transform.rotation = chunkDataSOs[levelId].rotation[i];
+
+                if(chunkToSaveTo == 1)
+                {
+                    spawnedChunk1.Add(spawnedObject);
+                }
+                else if(chunkToSaveTo == 2)
+                {
+                    spawnedChunk2.Add(spawnedObject);
+                }
+                else if(chunkToSaveTo == 3)
+                {
+                    spawnedChunk3.Add(spawnedObject);
+                }
+
+                /*
+                spawnedObject.GetComponent<NetworkTransform>().enabled = false;
+                spawnedObject.GetComponent<NetworkObject>().des = false;
+                spawnedObject.transform.parent = spawnedHolderParent.transform;
+                spawnedObject.GetComponent<NetworkObject>().enabled = true;
+                spawnedObject.GetComponent<NetworkTransform>().enabled = true;
+                */
                 spawnedObject.SetActive(true);
             }
             else
             {
-                Debug.Log("solid");
                 spawnedObject = GetPooledSolid();
                 if (spawnedObject == null) return;
                 spawnedObject.transform.localPosition = nextStartPos + chunkDataSOs[levelId].position[i];
                 spawnedObject.transform.localScale = chunkDataSOs[levelId].scale[i];
                 spawnedObject.transform.rotation = chunkDataSOs[levelId].rotation[i];
+
+                if (chunkToSaveTo == 1)
+                {
+                    spawnedChunk1.Add(spawnedObject);
+                }
+                else if (chunkToSaveTo == 2)
+                {
+                    spawnedChunk2.Add(spawnedObject);
+                }
+                else if (chunkToSaveTo == 3)
+                {
+                    spawnedChunk3.Add(spawnedObject);
+                }
+
+                /*
+                spawnedObject.GetComponent<NetworkObject>().enabled = false;
+                spawnedObject.transform.parent = spawnedHolderParent.transform;
+                spawnedObject.GetComponent<NetworkObject>().enabled = true;
+                */
                 spawnedObject.SetActive(true);
             }
             i++;
         }
 
+        levelSpawnOrder.Add(amountOfSpawnedLevels);
         levelIdOrder.Add(levelId);
- 
+
+        amountOfSpawnedLevels++;
+
         oldStartPos = nextStartPos;
         nextStartPos = oldStartPos + Vector3.up * chunkDataSOs[levelId].height;
         heightNextSpawn = oldStartPos.y;
