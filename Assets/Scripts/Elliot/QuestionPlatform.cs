19ab0c96034a73843e9f172554f145d95893ef4b
ChristofferNL <85027479+ChristofferNL@users.noreply.github.com>
2023-08-24 14:48:51 +0200

    shortened rope

--- Assets/Scripts/Elliot/QuestionPlatform.cs
+++ Assets/Scripts/Elliot/QuestionPlatform.cs
@@ -7,11 +7,17 @@ public class QuestionPlatform : MonoBehaviour
     [SerializeField] int amountOfPlayersColliding = 0;
     public QuestionManager questionManager;
 
+	bool hasSpawnedQuestion;
+
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (collision.gameObject.layer == 7) { amountOfPlayersColliding++; }
 
-		if (amountOfPlayersColliding >= 2) { questionManager.OpenNextQuestion_ServerRpc(); }
+		if (amountOfPlayersColliding >= 2) 
+		{ 
+			questionManager.OpenNextQuestion_ServerRpc(); 
+			hasSpawnedQuestion = true;
+		}
 	}
 
 	private void OnTriggerExit2D(Collider2D collision)
