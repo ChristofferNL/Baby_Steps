827f4b4d9ef162d36b6314bb4ede41aae2ee0b84
Potter72 <elliot.potter72@gmail.com>
2023-08-24 10:38:48 +0200

    fixed level generation

--- Assets/Scripts/Elliot/GenerateLevelChunkData.cs
+++ Assets/Scripts/Elliot/GenerateLevelChunkData.cs
@@ -7,6 +7,7 @@ using UnityEngine.PlayerLoop;
 using System.IO;
 using UnityEditor.TerrainTools;
 using Mono.Cecil.Cil;
+using UnityEditor.Rendering;
 
 /*
 [CustomEditor(typeof(GenerateLevelChunkData))]
@@ -25,6 +26,8 @@ public class GenerateLevelChunkData : MonoBehaviour
 {
     [SerializeField] Transform bottomLeft;
     [SerializeField] Transform topOfLevel;
+    private int currentSaveId;
+    [SerializeField] float debugFloat;
     private void Start()
     {
         GetLevelData();
@@ -56,12 +59,25 @@ public class GenerateLevelChunkData : MonoBehaviour
 
         for (int i = 0; i < transform.childCount; i++)
         {
-            if (transform.GetChild(i).CompareTag("PassThroughPlatform") || transform.GetChild(i).CompareTag("SolidPlatform"))
+            foreach (Transform child in transform.GetChild(i))
+            {
+                foreach (Transform child2 in child)
+                {
+                    if (child2.CompareTag("PassThroughPlatform") || child2.CompareTag("SolidPlatform") && !child.GetComponent<QuestionPlatform>() || child2.GetComponent<QuestionPlatform>())
+                    {
+                        numOfPlatforms++;
+                    }
+                }
+                if (child.CompareTag("PassThroughPlatform") && !child.GetComponentInChildren<QuestionPlatform>() || child.CompareTag("SolidPlatform") && !child.GetComponentInChildren<QuestionPlatform>())
+                {
+                    numOfPlatforms++;
+                }
+            }
+            if (transform.GetChild(i).CompareTag("PassThroughPlatform") && !transform.GetChild(i).GetComponentInChildren<QuestionPlatform>() || transform.GetChild(i).CompareTag("SolidPlatform") && !transform.GetChild(i).GetComponentInChildren<QuestionPlatform>())
             {
                 numOfPlatforms++;
             }
         }
-
         levelData.bottomLeft = bottomLeft.position;
         levelData.numberOfPlatforms = numOfPlatforms;
         levelData.height = topOfLevel.localPosition.y;
@@ -72,26 +88,143 @@ public class GenerateLevelChunkData : MonoBehaviour
         levelData.isPassThrough = new bool[numOfPlatforms];
         levelData.isQuestion = new bool[numOfPlatforms];
 
+        currentSaveId = 0;
+        /*
         for (int i = 0; i < transform.childCount; i++) 
         {
+            /*  ended up searching for question platforms twice, thought this only did it one level deep at first but it also searches grandchildren and their children and so on
             if (transform.GetChild(i).GetComponentInChildren<QuestionPlatform>())
             {
-                levelData.isQuestion[i - 1] = true;
+                levelData.isQuestion[currentSaveId] = true;
+            }
+            
+
+            if (transform.GetChild(i).childCount > 0)
+            {
+                if(transform.GetChild(i).GetChild(0).GetComponent<QuestionPlatform>())
+                levelData.isQuestion[currentSaveId] = true;
             }
 
             if (transform.GetChild(i).CompareTag("PassThroughPlatform"))
             {
-                levelData.isPassThrough[i -1] = true;
+                levelData.isPassThrough[currentSaveId] = true;
             }else if (transform.GetChild(i).CompareTag("SolidPlatform"))
             {
-                levelData.isPassThrough[i -1] = false;
+                levelData.isPassThrough[currentSaveId] = false;
+            }
+            
+            if((transform.GetChild(i).CompareTag("PassThroughPlatform")) 
+                || (transform.GetChild(i).CompareTag("SolidPlatform")))
+                Debug.LogError("youre here");
+                levelData.position[currentSaveId] = transform.GetChild(i).transform.localPosition;
+                levelData.scale[currentSaveId] = transform.GetChild(i).transform.localScale;
+                levelData.rotation[currentSaveId] = transform.GetChild(i).transform.rotation;
+                currentSaveId++;
             }
 
-            if(transform.GetChild(i).CompareTag("PassThroughPlatform") || transform.GetChild(i).CompareTag("SolidPlatform") || transform.GetChild(i).GetComponentInChildren<QuestionPlatform>())  //if its any type of platform
+            //GETS CHILDREN OF CHILDREN
+            if (transform.GetChild(i).childCount > 0)
             {
-                levelData.position[i - 1] = transform.GetChild(i).transform.localPosition;
-                levelData.scale[i - 1] = transform.GetChild(i).transform.localScale;
-                levelData.rotation[i - 1] = transform.GetChild(i).transform.rotation;
+                Transform childParent = transform.GetChild(i);
+                
+                for (int a = 0; a < childParent.childCount; a++)
+                {
+                    Debug.LogError("childcount: " + childParent.childCount);
+                    if (childParent.GetChild(a).GetChild(0).GetComponent<QuestionPlatform>() != null)
+                    {
+                        
+                        levelData.isQuestion[currentSaveId] = true;
+                    }
+
+                    if (childParent.GetChild(a).CompareTag("PassThroughPlatform"))
+                    {
+                        levelData.isPassThrough[currentSaveId] = true;
+                    }
+                    else if (childParent.GetChild(a).CompareTag("SolidPlatform"))
+                    {
+                        levelData.isPassThrough[currentSaveId] = false;
+                    }
+
+                    if (childParent.GetChild(a).CompareTag("PassThroughPlatform") || transform.GetChild(i).CompareTag("SolidPlatform") || transform.GetChild(i).GetComponentInChildren<QuestionPlatform>())  //if its any type of platform
+                    {
+                        levelData.position[currentSaveId] = transform.GetChild(i).transform.localPosition + childParent.GetChild(a).transform.localPosition;
+                        levelData.scale[currentSaveId] = transform.GetChild(i).transform.localScale + childParent.GetChild(a).transform.localPosition;
+                        levelData.rotation[currentSaveId] = transform.GetChild(i).transform.rotation * childParent.GetChild(a).transform.rotation;
+                        currentSaveId++;
+                    }
+                }
+            }
+            
+        }
+    */
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            foreach (Transform child in transform.GetChild(i))
+            {
+                foreach (Transform child2 in child)
+                {
+                    if (child2.GetComponent<QuestionPlatform>())
+                    {
+                        levelData.isQuestion[currentSaveId] = true;
+                        levelData.position[currentSaveId] = child2.parent.transform.position - transform.position;
+                        levelData.scale[currentSaveId] = child2.parent.transform.localScale;
+                        levelData.rotation[currentSaveId] = child2.parent.transform.rotation;
+                        currentSaveId++;
+                    }else if (child2.CompareTag("PassThroughPlatform"))
+                    {
+                        levelData.isPassThrough[currentSaveId] = true;
+                        levelData.position[currentSaveId] = child2.position - transform.position;
+                        levelData.scale[currentSaveId] = child2.localScale;
+                        levelData.rotation[currentSaveId] = child2.rotation;
+                        currentSaveId++;
+                    }
+                    else if(child2.CompareTag("SolidPlatform"))
+                    {
+                        levelData.isPassThrough[currentSaveId] = false;
+                        levelData.position[currentSaveId] = child2.position - transform.position;
+                        levelData.scale[currentSaveId] = child2.localScale;
+                        levelData.rotation[currentSaveId] = child2.rotation;
+                        currentSaveId++;
+                    }
+                }
+                if (child.CompareTag("PassThroughPlatform") && !child.GetComponentInChildren<QuestionPlatform>() || child.CompareTag("SolidPlatform") && !child.GetComponentInChildren<QuestionPlatform>())
+                {
+                    if(child.CompareTag("PassThroughPlatform") && !child.GetComponentInChildren<QuestionPlatform>())
+                    {
+                        levelData.isPassThrough[currentSaveId] = true;
+                        levelData.position[currentSaveId] = child.position - transform.position;
+                        levelData.scale[currentSaveId] = child.localScale;
+                        levelData.rotation[currentSaveId] = child.rotation;
+                        currentSaveId++;
+                    }
+                    else if(child.CompareTag("SolidPlatform") && !child.GetComponentInChildren<QuestionPlatform>())
+                    {
+                        levelData.isPassThrough[currentSaveId] = false;
+                        levelData.position[currentSaveId] = child.position - transform.position;
+                        levelData.scale[currentSaveId] = child.localScale;
+                        levelData.rotation[currentSaveId] = child.rotation;
+                        currentSaveId++;
+                    }
+                }
+            }
+            if (transform.GetChild(i).CompareTag("PassThroughPlatform") && !transform.GetChild(i).GetComponentInChildren<QuestionPlatform>() || transform.GetChild(i).CompareTag("SolidPlatform") && !transform.GetChild(i).GetComponentInChildren<QuestionPlatform>())
+            {
+                if (transform.GetChild(i).CompareTag("PassThroughPlatform") && !transform.GetChild(i).GetComponentInChildren<QuestionPlatform>())
+                {
+                    levelData.isPassThrough[currentSaveId] = true;
+                    levelData.position[currentSaveId] = transform.GetChild(i).transform.localPosition;
+                    levelData.scale[currentSaveId] = transform.GetChild(i).transform.localScale;
+                    levelData.rotation[currentSaveId] = transform.GetChild(i).transform.rotation;
+                    currentSaveId++;
+                }
+                else if (transform.GetChild(i).CompareTag("SolidPlatform") && !transform.GetChild(i).GetComponentInChildren<QuestionPlatform>())
+                {
+                    levelData.isPassThrough[currentSaveId] = false;
+                    levelData.position[currentSaveId] = transform.GetChild(i).transform.localPosition;
+                    levelData.scale[currentSaveId] = transform.GetChild(i).transform.localScale;
+                    levelData.rotation[currentSaveId] = transform.GetChild(i).transform.rotation;
+                    currentSaveId++;
+                }
             }
         }
 
